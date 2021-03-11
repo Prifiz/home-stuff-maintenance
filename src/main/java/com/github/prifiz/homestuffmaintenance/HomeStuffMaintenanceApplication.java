dfefb2181ab0b076bc8f002ccf0a5e91ac31de82
Prifiz <baranov.v.e@yandex.ru>
2021-03-11 04:13:01 +0400

    Initial structure

---
+++ src/main/java/com/github/prifiz/homestuffmaintenance/HomeStuffMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.github.prifiz.homestuffmaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeStuffMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeStuffMaintenanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/prifiz/homestuffmaintenance/HomeStuffMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.prifiz.homestuffmaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeStuffMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
